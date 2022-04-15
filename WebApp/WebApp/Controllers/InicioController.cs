0a90e524b5fbd0ea470ff97519f3b2e174bc4a08
Alexis Quinones <e.xis@hotmail.com>
2022-04-15 10:00:37 -0500

    cambios

---
+++ WebApp/WebApp/Controllers/InicioController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class InicioController : Controller
+    {
+        readonly GruposanchoEntities db = new GruposanchoEntities();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public PartialViewResult CrearEmpleado()
+        {
+            ViewBag.tipodocumento = db.TipoDocumento.ToList();
+            ViewBag.genero = db.Genero.ToList();
+            return PartialView();
+        }
+
+        [HttpPost]
+        public JsonResult CrearEmpleado(string nombre, string apellidos, int tipodocumento, string numerodocumento, string correo,
+            int genero, int edad)
+        {
+
+
+
+            ViewBag.tipodocumento = db.TipoDocumento.ToList();
+            ViewBag.genero = db.Genero.ToList();
+            return Json(0);
+        }
+    }
+}
